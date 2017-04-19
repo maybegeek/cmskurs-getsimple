c862d008aa9e5fb2edeb9454f0eda78f963289d1
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 11:40:48 +0200

    copied default template to two new templates to be
    Weitere Template-Dateien werden angelegt. Hierzu wurde das default template
    (template.php) kopiert und mit anderem Namen Versehen. Dieser Dateiname
    taucht später (bei der verwendeten GS Version) im Auswahldialog unter
    Page Options auf. Einen sinnvollen Dateinamen verwenden ist ratsam.

---
+++ theme/UnserTheme/ohne-sidebar.php
@@ -0,0 +1,46 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File:     template.php
+* @Package:  GetSimple
+* @Action:   UnserTheme ist ein Beispiel-Theme für GetSimple
+*
+*****************************************************/
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php get_header(); ?>
+  <meta name="robots" content="index, follow">
+  <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
+  <link rel="stylesheet" href="<?php get_theme_url(); ?>/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Average">
+</head>
+<body id="<?php get_page_slug(); ?>" class="grid w960">
+
+  <header class="row">
+    <h1 class="c12"><a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a></h1>
+  </header>
+
+  <nav class="row">
+    <ul class="c12">
+      <?php get_navigation(return_page_slug()); ?>
+    </ul>
+  </nav>
+
+  <main class="row">
+    <div class="page-content c8">
+      <h2><?php get_page_title(); ?></h2>
+      <?php get_page_content(); ?>
+      <p class="page-meta"><time datetime="<?php get_page_date('c'); ?>"><?php get_page_date('j. F Y'); ?></time></p>
+    </div>
+    <aside class="c4">
+      <?php get_component('sidebar');  ?>
+    </aside>
+  </main>
+
+  <?php get_footer(); ?>
+</body>
+</html>
