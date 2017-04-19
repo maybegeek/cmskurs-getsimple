69e073527f737a6c689f74895258fd90558cde0b
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 11:32:20 +0200

    template with grid, web fonts ...
    neogridic als mini css framework, web fonts eingebunden. Dies
    hier wird das default Template, ein abweichendes werden wir nacher noch
    erstellen. Nach der Installation von GetSimple sollten nun noch einige
    Seiten oberster Ebene hinzugefügt werden, damit das Menü auch
    sichtbar wird. (Im Beispiel: Startseite, Seite2, Seite3).

--- theme/UnserTheme/template.php
+++ theme/UnserTheme/template.php
@@ -10,33 +10,33 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <!-- Site Title -->
   <title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php get_header(); ?>
   <meta name="robots" content="index, follow">
   <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
   <link rel="stylesheet" href="<?php get_theme_url(); ?>/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Average">
 </head>
-<body id="<?php get_page_slug(); ?>" >
+<body id="<?php get_page_slug(); ?>" class="grid w960">
 
-  <header>
-    <h1><a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a></h1>
+  <header class="row">
+    <h1 class="c12"><a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a></h1>
   </header>
 
-  <nav>
-    <ul>
+  <nav class="row">
+    <ul class="c12">
       <?php get_navigation(return_page_slug()); ?>
     </ul>
   </nav>
 
-  <main>
-    <div class="page-content">
+  <main class="row">
+    <div class="page-content c8">
       <h2><?php get_page_title(); ?></h2>
       <?php get_page_content(); ?>
       <p class="page-meta"><time datetime="<?php get_page_date('c'); ?>"><?php get_page_date('j. F Y'); ?></time></p>
     </div>
-    <aside>
+    <aside class="c4">
       <?php get_component('sidebar');  ?>
     </aside>
   </main>
