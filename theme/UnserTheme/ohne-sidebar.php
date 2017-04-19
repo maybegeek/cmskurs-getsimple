9084385f48e8ec60fb30a8cdec8f251013f40df9
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 12:04:52 +0200

    only <main> element changes in the three templates
    Nur der <main>-Bereich ändert sich über die drei template-Dateien
    hinweg, deswegen ist auch nur dieser in den entsprechenden Dateien.
    
    Alles andere ist ausgekapselt und kann damit leichter bearbeitet
    werden.

--- theme/UnserTheme/ohne-sidebar.php
+++ theme/UnserTheme/ohne-sidebar.php
@@ -6,29 +6,7 @@
 * @Action:   UnserTheme: Template ohne sidebar/component, volle Breite
 *
 *****************************************************/
-?><!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php get_header(); ?>
-  <meta name="robots" content="index, follow">
-  <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
-  <link rel="stylesheet" href="<?php get_theme_url(); ?>/style.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Average">
-</head>
-<body id="<?php get_page_slug(); ?>" class="grid w960">
-
-  <header class="row">
-    <h1 class="c12"><a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a></h1>
-  </header>
-
-  <nav class="row">
-    <ul class="c12">
-      <?php get_navigation(return_page_slug()); ?>
-    </ul>
-  </nav>
+?><?php include('tmpl-oben.inc.php'); ?>
 
   <main class="row">
     <div class="page-content c12">
@@ -38,6 +16,4 @@
     </div>
   </main>
 
-  <?php get_footer(); ?>
-</body>
-</html>
+<?php include('tmpl-unten.inc.php'); ?>
