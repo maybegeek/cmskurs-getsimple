fc8b2a3f6aff4e4cc44b4a35087f2ccde870f09b
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 09:28:00 +0200

    minimal template
    html5

--- theme/UnserTheme/template.php
+++ theme/UnserTheme/template.php
@@ -1,66 +1,46 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
 /****************************************************
 *
-* @File:       template.php
-* @Package:    GetSimple
-* @Action:    Cardinal theme for GetSimple CMS
+* @File:     template.php
+* @Package:  GetSimple
+* @Action:   UnserTheme ist ein Beispiel-Theme für GetSimple
 *
 *****************************************************/
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html>
 <head>
-
+  <meta charset="utf-8">
   <!-- Site Title -->
   <title><?php get_page_clean_title(); ?> &lt; <?php get_site_name(); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php get_header(); ?>
-  <meta name="robots" content="index, follow" />
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/style.css" media="all" />
-
+  <meta name="robots" content="index, follow">
+  <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
+  <link rel="stylesheet" href="<?php get_theme_url(); ?>/style.css">
 </head>
 <body id="<?php get_page_slug(); ?>" >
 
-<div id="wrapper">
+  <header>
+    <h1><a href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a></h1>
+  </header>
 
-  <div id="header">
-
-    <ul id="nav">
+  <nav>
+    <ul>
       <?php get_navigation(return_page_slug()); ?>
     </ul>
+  </nav>
 
-    <span class="logo2"><?php get_site_name(); ?></span>
-    <a class="logo" href="<?php get_site_url(); ?>"><?php get_site_name(); ?></a>
-
-  </div><!-- end header -->
-
-  <div id="content">
-    <h1><?php get_page_title(); ?></h1>
-      <div id="page-content">
-        <div class="page-text">
-          <?php get_page_content(); ?>
-          <p class="page-meta">Published on &nbsp;<span><?php get_page_date('F jS, Y'); ?></span></p>
-        </div>
-      </div>
-  </div>
-
-  <div id="sidebar">
-    <div class="section">
+  <main>
+    <div class="page-content">
+      <h2><?php get_page_title(); ?></h2>
+      <?php get_page_content(); ?>
+      <p class="page-meta"><time datetime="<?php get_page_date('c'); ?>"><?php get_page_date('j. F Y'); ?></time></p>
+    </div>
+    <aside>
       <?php get_component('sidebar');  ?>
-    </div>
-    <div class="section credits">
-      <p><?php echo date('Y'); ?> - <strong><?php get_site_name(); ?></strong></p>
-      <p>
-        Cardinal Theme by <a href="http://www.cagintranet.com" >Cagintranet</a><br />
-        <?php get_site_credits(); ?>
-      </p>
-    </div>
-  </div>
-
-  <div class="clear"></div>
+    </aside>
+  </main>
 
   <?php get_footer(); ?>
-
-</div><!-- end wrapper -->
 </body>
 </html>
