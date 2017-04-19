3f6a496e24caa86732128ac32648c679f8466a07
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 13:30:02 +0200

    add functions.php to theme (optional)
    In der functions.php können eigene Funktionen zum Theme mitgegeben
    werden. Beispielhaft hier aber auskommentiert ein Filter, welcher den
    content in Versalien wandelt.

---
+++ theme/UnserTheme/functions.php
@@ -0,0 +1,23 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File:     functions.php
+* @Package:  GetSimple
+* @Action:   UnserTheme: zusätzliche Funktionen
+*
+*****************************************************/
+
+//// eigene plugin hooks und filter koennen
+//// genutzt werden:
+//// http://get-simple.info/wiki/plugins:hooks_filters
+//// und in der functions.php mit dem jeweiligen
+//// theme verbandelt werden.
+////
+//add_filter('content', 'eigener_filter');
+//
+//function eigener_filter($content) {
+//  $versalien = strtoupper($content);
+//  return $versalien;
+//}
+
+?>
